42744c08eb3153d17ba380ae3a48b3492da56359
Wien Allen Reyes <reyeswa@students.nu-fairview.edu.ph>
2025-07-16 18:42:50 +0800

    raw

---
+++ JDE_WORKS/index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="home.css">
+    <title>JDE WORKS OF OUR HANDS</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-md">
+        <div class="container-fluid">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Services</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php#Category">Products</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php#About">About Us</a></li>
+                    <li class="nav-item"><a class="nav-link" href="index.php#Contact">Contact us</a></li>
+                </ul>
+            </div>
+
+             <a class="navbar-brand" href="index.php"><img src="assets/img/logojd.png" alt="JDE Works of Our Hands Logo" class="logo"></a>
+
+            <div class="navbar-right button-container">
+                <a href="signup.php" class="signup">Sign Up</a>
+                <a href="login.php" class="login">Login</a>
+            </div>
+        </div>
+    </nav>
+
+    <div class="hero-image">
+        <div class="hero-content">
+            <h2>Welcome to</h2>
+            <h1>JDE Works <br>of Our Hands</h1>
+            <a href=".php"><button class="btn">ORDER</button></a>
+        </div>
+    </div>
+
+    <div class="Products">
+        <div class="container">
+            <h1>Other Products</h1>
+        </div>
+    </div>
+
+    <div class="Our-Story">
+        <div class="container-story">
+            <h1>Our Story</h1>
+        </div>
+    </div>
+
+    <div class="Contact">
+        <div class="container">
+            <h1>Contact us</h1>
+        </div>
+    </div>
+
+    <footer><center><p>JDE Works Of Our Hands</p></center></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
