6e420a8611fc5b6f64bfc7f98029f9dac3ea512f
Wien Allen Reyes <reyeswa@students.nu-fairview.edu.ph>
2025-07-19 11:23:11 +0800

    Add files via upload
    index/home page update

--- JDE_WORKS/index.php
+++ JDE_WORKS/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="home.css">
-    <title>JDE WORKS OF OUR HANDS</title>
+    <title>JDE WORK OF OUR HANDS</title>
 </head>
 <body>
     <nav class="navbar navbar-expand-md">
@@ -24,7 +24,11 @@
                 </ul>
             </div>
 
-             <a class="navbar-brand" href="index.php"><img src="assets/img/logojd.png" alt="JDE Works of Our Hands Logo" class="logo"></a>
+            <div class="navbar-brand">
+                <a href="index.php"><img src="assets/img/logojd.png" alt="JDE Works of Our Hands Logo" class="logo"></a>
+            </div>
+             
+
 
             <div class="navbar-right button-container">
                 <a href="signup.php" class="signup">Sign Up</a>
@@ -35,27 +39,103 @@
 
     <div class="hero-image">
         <div class="hero-content">
-            <h2>Welcome to</h2>
-            <h1>JDE Works <br>of Our Hands</h1>
+            <div class="container-hero">
+            <br><br>
+            <h1>JDE Works of Our Hands</h1><br><br><br>
             <a href=".php"><button class="btn">ORDER</button></a>
+            <br><br>
+            </div>
         </div>
     </div>
 
-    <div class="Products">
-        <div class="container">
-            <h1>Other Products</h1>
+  <div class="Products">
+    <div class="container">
+        <h1>Other Products</h1><br><br>
+
+        <div class="product-grid"> 
+            <div class="product-item">
+                <img src="assets/img/pr1.png">
+                <div class="item-text">
+                    <h3>Pre-made <br>Uniform</h3>
+                </div>
+            </div>
+
+            <div class="product-item">
+                <img src="assets/img/pr2.png">
+                <div class="item-text">
+                    <h3>Custom-made <br>Uniform</h3>
+                </div>
+            </div>
+
+            <div class="product-item">
+                <img src="assets/img/pr3.png">
+                <div class="item-text">
+                    <h3>Polo Shirt</h3>
+                </div>
+            </div>
+
+            <div class="product-item">
+                <img src="assets/img/pr4.png">
+                <div class="item-text">
+                    <h3>T-shirt</h3>
+                </div>
+            </div>
+
+            </div> 
         </div>
     </div>
 
     <div class="Our-Story">
-        <div class="container-story">
+        <div class="container">
             <h1>Our Story</h1>
+
+            <div class="story-grid">
+                <div class="story-item">
+                    <p>
+                        JDE Work of Our Hands has proudly served the Caloocan City community for 12 years, specializing in high-quality tailoring services. Located in Adeline Homes, we initially focused on crafting durable and comfortable school uniforms, becoming a trusted provider for local families. Over the years, our expertise has expanded to include general alterations, catering to the diverse tailoring needs of our neighborhood. Our deep understanding of the local market allows us to provide tailored solutions and build lasting relationships with our customers. 
+                    </p>
+                </div>
+
+                <div class="story-item">
+                    <img src="assets/img/st.png">
+                </div>
+                <div class="story-item">
+                    <img src="assets/img/st1.png">
+                </div>
+                <div class="story-item">
+                    <img src="assets/img/st2.png">
+                </div>
+            </div>
         </div>
     </div>
 
     <div class="Contact">
         <div class="container">
             <h1>Contact us</h1>
+
+              <div class="location-container">
+                <div class="location-info">
+    
+                    <h3>Location</h3>
+                    <p>
+                        Hilltop Branch <br>
+                        Esperanca cor. Caridad st. Hilltop subd. <br>
+                        Greater Lagro, Quezon City <br>
+                    </p>
+
+                    <h3>Contact Numbers</h3>
+                    <p><a href="tel:(02)83591585">
+                        0933-598-7864 / 0997-892-7142 / 0907-585-9069 / 0905-876-3368 /8651-3605
+                    </a></p>
+                </div>
+
+                <div class="location-map">
+                    <iframe 
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d603.3558382712196!2d121.06990730426064!3d14.734815328185528!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3397b155c1031841%3A0x628522affbb6df1b!2sJDE%20Work%20of%20Our%20Hands!5e1!3m2!1sen!2sph!4v1752894339893!5m2!1sen!2sph"
+                    width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                    </iframe>
+                </div>
+            </div>
         </div>
     </div>
 
